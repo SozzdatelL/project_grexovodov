1211f5217a713a48aefa12798d5f5915b80d1094
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-08 16:23:23 +0300

    Update pz_11.1.py

--- PZ_11/pz_11.1.py
+++ PZ_11/pz_11.1.py
@@ -1,1 +1,9 @@
+#В последовательности на n целых чисел найти и вынести: макс. среди отриц.элементы кратные двум и их сумму.
 
+a = [5, -2, 8, -1, 3, 7, -10, 4, -6]
+
+neg_max = max((x for x in a if x < 0), default="no")
+evens = [x for x in a if x % 2 == 0]
+print(neg_max)
+print(evens)
+print(sum(evens))
