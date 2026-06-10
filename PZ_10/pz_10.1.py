2e0ffd7c018ff95afbe7ca96ae83d79bdee2c954
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-10 16:43:18 +0300

    Update pz_10.1.py

--- PZ_10/pz_10.1.py
+++ PZ_10/pz_10.1.py
@@ -1,1 +1,21 @@
+#Создать txt → записать количество, элементы в обратном порядке и сумму второй половины
+
+
+f = open("numbers.txt", "w")
+
+a = list(map(int, input("Введите числа: ").split()))
+
+f.write("Исходные данные:\n")
+f.write(" ".join(map(str, a)) + "\n")
+
+f.write("Количество элементов:\n")
+f.write(str(len(a)) + "\n")
+
+f.write("Элементы в обратном порядке:\n")
+f.write(" ".join(map(str, a[::-1])) + "\n")
+
+f.write("Сумма элементов последней половины:\n")
+f.write(str(sum(a[len(a)//2:])))
+
+f.close()
 
