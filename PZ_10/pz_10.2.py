1c4d465e133e953d6d29cd2c9d10536b0413fa33
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-10 17:07:23 +0300

    Update pz_10.2.py

--- PZ_10/pz_10.2.py
+++ PZ_10/pz_10.2.py
@@ -1,1 +1,22 @@
+#Вывести файл, посчитать заглавные буквы, создать новый файл с автором и названием
 
+f = open("text18-10.txt", "r", encoding="utf-8")
+
+text = f.read()
+print(text)
+
+k = 0
+for i in text:
+    if i.isupper():
+        k += 1
+
+print("Количество больших букв:", k)
+
+new = open("new_text.txt", "w", encoding="utf-8")
+
+new.write(text)
+new.write("\n\nАвтор: М. Ю. Лермонтов")
+new.write("\nНазвание: Бородино")
+
+f.close()
+new.close()
