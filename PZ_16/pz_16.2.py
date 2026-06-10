1fd3f04608e3fd0b2441bf8b7864340b617528ca
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-10 15:43:21 +0300

    Update pz_16.2.py

--- PZ_16/pz_16.2.py
+++ PZ_16/pz_16.2.py
@@ -1,1 +1,17 @@
+#написать код что бы в выводе была кличка собаки, порода, окрас.
+class Zhivotnoe:
+    def __init__(self, vid, kolichestvo_lap, tsvet_shersti):
+        self.vid = vid
+        self.kolichestvo_lap = kolichestvo_lap
+        self.tsvet_shersti = tsvet_shersti
 
+
+class Sobaka(Zhivotnoe):
+    def __init__(self, vid, kolichestvo_lap, tsvet_shersti, klichka, poroda):
+        super().__init__(vid, kolichestvo_lap, tsvet_shersti)
+        self.klichka = klichka
+        self.poroda = poroda
+
+
+dog = Sobaka("sobaka", 4, "ryzhiy", "Sharik", "Dvornaga")
+print(dog.klichka, dog.poroda, dog.vid)
