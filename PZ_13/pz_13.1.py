ae5d8a7eb7f560b459c76fd31b883d96f838cbaa
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-24 06:05:11 +0300

    Update pz_13.1.py

--- PZ_13/pz_13.1.py
+++ PZ_13/pz_13.1.py
@@ -1,7 +1,15 @@
 #В исходном текстовом файле (Dostoevsky.txt) найти все фамилии с инициалами (например, А. Ф. Куманиной и т.п.).
 
 import re
-with open('/home/workdir/attachments/Dostoevsky.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-matches = re.findall(r'\b([А-ЯЁ]\.\s*[А-ЯЁ]\.?\s*[А-ЯЁ][а-яё]*)\b', text)
-print(matches)
+
+with open("Dostoevsky.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+pattern = r"[А-ЯЁ]\.\s?[А-ЯЁ]\.\s?[А-Яа-яЁё]+"
+
+matches = re.findall(pattern, text)
+
+print("Найденные фамилии с инициалами:\n")
+
+for name in matches:
+    print(name)
