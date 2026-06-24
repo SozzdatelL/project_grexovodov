e85e50071cc1e1aaacf39694f3f94114ce106da7
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-24 03:51:30 +0300

    Update pz_8.1.py

--- PZ_8/pz_8.1.py
+++ PZ_8/pz_8.1.py
@@ -1,4 +1,15 @@
 #1. Проверьте наличие значения 200 в sample_dict = {'a': 100, 'b': 200, 'c': 300}.
+
 sample_dict = {'a': 100, 'b': 200, 'c': 300}
 
-print(200 in sample_dict.values())
+found = False
+
+for value in sample_dict.values():
+    if value == 200:
+        found = True
+        break
+
+if found:
+    print("Значение 200 найдено")
+else:
+    print("Значение 200 не найдено")
