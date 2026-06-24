bf28404c62b75a7ab99bf7a2155d183a25e04cca
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-24 04:11:01 +0300

    Update pz_16.1.py

--- PZ_16/pz_16.1.py
+++ PZ_16/pz_16.1.py
@@ -1,12 +1,14 @@
 #Создайте класс "Здание" с атрибутами "адрес" и "количество этажей". Напишите метод, который выводит информацию о здании в формате "Адрес: адрес, Количество этажей: этажи".
+
 class Building:
     def __init__(self, address, floors):
         self.address = address
         self.floors = floors
 
     def show_info(self):
-        print(f"Address: {self.address}, Number of floors: {self.floors}")
+        print(f"Адрес: {self.address}, Количество этажей: {self.floors}")
 
 
-building = Building("123 Main St", 5)
-building.show_info()
+building1 = Building("ул. Ленина, 15", 9)
+
+building1.show_info()
