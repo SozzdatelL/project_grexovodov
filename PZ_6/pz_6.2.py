1c159c88be9ffa41cb48ea04aefef78f71c13901
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-27 01:56:12 +0300

    Update pz_6.2.py

--- PZ_6/pz_6.2.py
+++ PZ_6/pz_6.2.py
@@ -1,12 +1,32 @@
 #2. Дан список размера N. Найти количество его промежутков монотонности (то есть участков, на которых его элементы возрастают или убывают).
 
-def task2(A):
-    if len(A) < 2: return 0
-    cnt = 1
-    for i in range(1, len(A)-1):
-        if (A[i]-A[i-1]) * (A[i+1]-A[i]) < 0:
-            cnt += 1
-    return cnt
+N = int(input())
+A = list(map(int, input().split()))
 
-A = [1, 3, 5, 4, 2, 7, 8]
-print(task2(A))
+if N <= 1:
+    print(0)
+else:
+    count = 1
+
+    i = 1
+    while i < N and A[i] == A[i - 1]:
+        i += 1
+
+    if i == N:
+        print(1)
+    else:
+        direction = 1 if A[i] > A[i - 1] else -1
+
+        for j in range(i + 1, N):
+            if A[j] > A[j - 1]:
+                new_dir = 1
+            elif A[j] < A[j - 1]:
+                new_dir = -1
+            else:
+                continue
+
+            if new_dir != direction:
+                count += 1
+                direction = new_dir
+
+        print(count)
