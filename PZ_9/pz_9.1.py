69c8159b66679cca4eeea1395bfbd68403572afb
mgsh1shhh14 <grehovodov.maxim@gmail.com>
2026-06-29 08:58:20 +0300

    Update pz_9.1.py

--- PZ_9/pz_9.1.py
+++ PZ_9/pz_9.1.py
@@ -1,39 +1,26 @@
 #Туристические агентства предлагают следующие туры. Вояж – Мексика,Канада,Израиль,Италия,США. РейнаТур – Англия,Япония,Канада,ЮАР. Радуга – США,Испания,Швеция, Австралия. Определить:1. в каких турагенствах можно приобрести туры в Японию.2. в каких турагенствах нельзя приобрести туры в ЮАР.3. полный список всех туров.
 
-agencies = {
-    "Вояж": {"Мексика", "Канада", "Израиль", "Италия", "США"},
-    "РейнТур": {"Англия", "Япония", "Канада", "ЮАР"},
-    "Радуга": {"США", "Испания", "Швеция", "Австралия"}
-}
+voyazh = {"Мексика", "Канада", "Израиль", "Италия", "США"}
+reina = {"Англия", "Япония", "Канада", "ЮАР"}
+raduga = {"США", "Испания", "Швеция", "Австралия"}
 
 
-
-japan_tours = set()
-
-for agency, countries in agencies.items():
-    if "Япония" in countries:
-        japan_tours.add(agency)
-
-print("1. Туры в Японию можно приобрести в:")
-print(", ".join(sorted(japan_tours)))
+print("Япония есть:")
+if "Япония" in voyazh:
+    print("Вояж")
+if "Япония" in reina:
+    print("РейнаТур")
+if "Япония" in raduga:
+    print("Радуга")
 
 
-
-not_south_africa = set()
-
-for agency, countries in agencies.items():
-    if "ЮАР" not in countries:
-        not_south_africa.add(agency)
-
-print("\n2. Туры в ЮАР нельзя приобрести в:")
-print(", ".join(sorted(not_south_africa)))
+print("ЮАР нет:")
+if "ЮАР" not in voyazh:
+    print("Вояж")
+if "ЮАР" not in reina:
+    print("РейнаТур")
+if "ЮАР" not in raduga:
+    print("Радуга")
 
 
-
-all_tours = set()
-
-for countries in agencies.values():
-    all_tours.update(countries)
-
-print("\n3. Полный список всех туров:")
-print(", ".join(sorted(all_tours)))
+print("Все туры:", voyazh | reina | raduga)
